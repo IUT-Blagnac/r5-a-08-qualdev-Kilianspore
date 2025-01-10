6ffdecf29a542aa46285967df332f4ad7e049490
Kilianspore <84775788+Kilianspore@users.noreply.github.com>
2025-01-10 16:45:31 +0100

    tp4

--- cucumber_demo/src/test/java/dojo/RunCucumberTest.java
+++ cucumber_demo/src/test/java/dojo/RunCucumberTest.java
@@ -2,9 +2,11 @@ package dojo;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)  
+@CucumberOptions(plugin = {"json:target/cucumber.json"} )
 public class RunCucumberTest {
 }
 
